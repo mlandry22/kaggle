a6d3c502f03ff40794c5cf7916c984b3ec984564
Mark Landry <mark.a.landry@gmail.com>
2015-03-26 10:17:22 -0500

    Update histogram_starter_model.R

--- will_it_rain/histogram_starter_model.R
+++ will_it_rain/histogram_starter_model.R
@@ -1,3 +1,17 @@
+#####################################################
+## Starter Script for Kaggle Will it Rain competition
+#####################################################
+##
+## This is a script to show a simple alternative to
+##  using a sigmoid distribution.
+## It is essentially a histogram prediction, using
+##  coarse measurements of the same RR1 value used
+##  in the provided benchmark.
+##
+## Performance: 0.00872817
+## Relative performance: 37th out of 189; improves upon 
+## provided benchmark (0.01177621), and using all 1's (0.01017651)
+######################################################
 start<-Sys.time()
 #setwd("directory where the data files are located, if not getwd()")
 
@@ -6,23 +20,26 @@ start<-Sys.time()
 library(data.table)	
 train<-fread("train_2013.csv",nrows=250000,select=c("RR1","Expected"))
 
-## parse the readings inside each RR1 value, which are space-delimited
+## parse the readings inside each RR1 value, which are space-delimited, and take the mean of all values
 rr1.mean<-unlist(lapply(train[,RR1], function(x) mean(pmax(0,pmin(70,as.numeric(strsplit(x," ")[[1]]))))))
+## in case there are any NAs, replace with 0
 rr1.mean[is.na(rr1.mean)]<-0
 
 ## look at the data
 ## notice most observations are 0, and after that most between 0 and 0.5
 ## so using typical quantiles or breaks will merely subdivide a really small range of measurements
 ## So instead, we break off the most popular buckets, and then divide the remainder of the RR1 distribution
+## So we are asking for the ranges of RR1, but forcing the first and second range starts to be 0 and 0.5
 table(round(rr1.mean,1))
 breaks<-c(-Inf,0,quantile(rr1.mean[rr1.mean>0.5],c(0:8)/8))
 names(breaks)<-NULL
-n<-length(breaks)-1
+##store the break length to use throughout the rest of the code, in case you want to measure more breaks than 10
+n<-length(breaks)-1   
 
 ## create a matrix to store predictions for key points along the RR1 distribution
 observations<-matrix(,n,10)
 
-## now override the default 100% with empirical values
+## now fill in with empirical values
 for(i in 1:n){	
 	## for the various cutpoints (breaks) in the RR1 distribution, measure the frequency
 	##	of the bottom 10 levels of the target value (0mm,1mm,2mm,...9mm)
@@ -58,10 +75,11 @@ colnames(predictions)<-c("Id",paste0("Predicted",(seq(1:70)-1)))
 ## override the 100% values with our table lookup; want to improve this? smooth the edge
 for(i in 1:n){predictions[rr1.mean>breaks[i]&rr1.mean<=breaks[i+1],2:(n+1)]<-as.data.frame(t(observations[i,1:n]))}
 
-## output predictions; outputs as 188MB, but compresses to 6.8MB (lot of 1's)
-options("scipen"=100, "digits"=8)
+## output predictions; outputs as 184MB, but compresses to 4.3MB (lot of 1's)
+##scipen turns off scientific notation (for ID values such as 100000); Kaggle rejects those
+options("scipen"=100, "digits"=8)  
 write.table(predictions,"histogram_benchmark.csv",quote = FALSE, sep = ",",row.names=FALSE)
 
-##how long did it take (3.3 minutes on a Windows i5 with 6GB memory and slow HDD)
+##how long did it take (2.9 minutes on a Windows i5 with 6GB memory and slow HDD)
 stop<-Sys.time()
 stop-start
